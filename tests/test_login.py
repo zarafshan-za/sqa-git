192795893038df8eb0bc7645c3c24babeb60b80f
Zarafshan <zarjumand.za@gmail.com>
2026-04-19 17:02:22 +0500

    main: add logout test

--- tests/test_login.py
+++ tests/test_login.py
@@ -2,3 +2,8 @@ def test_login_success():
     # SQA Engineer A's version (on main)
     response_status = 200
     assert response_status == 200, "Login should return 200"
+
+def test_logout():
+    # Engineer A also added this
+    is_logged_out = True
+    assert is_logged_out, "Logout failed"
